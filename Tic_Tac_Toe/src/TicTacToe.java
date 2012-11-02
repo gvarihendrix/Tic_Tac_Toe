aa5d4827df272f8bb4947aca9bcf62743808c642
Ingvar Sigurðsson <gvarihendrix@gmail.com>
2012-11-02 14:12:01 +0000

    Fysti java klasinn Eftir Ingvar Sigurðsson

---
+++ Tic_Tac_Toe/src/TicTacToe.java
@@ -0,0 +1,15 @@
+/**
+ * @author FAB 5
+ * @since 02/11/12
+ * @version 1.0
+ */
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        new TicTacToe();
+    }
+
+    public TicTacToe(){
+        System.out.println("This is the new tic tac toe game, please implement me");
+    }
+}
